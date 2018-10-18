5a7b67f4e3ca2cff43a3642bf0648b0e2ab32a5c
Matt Cholick <cholick@gmail.com>
2018-10-18 09:28:02 -0700

    Update bazaar tile

--- bazaar-tile/create-tile.sh
+++ bazaar-tile/create-tile.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-kibosh_version=0.0.75
+kibosh_version=0.0.106
 kibosh_release_path=./resources/kibosh-release-${kibosh_version}.tgz
 if [ -f "${kibosh_release_path}" ]; then
     echo " kibosh release tgz already exists, skipping download"
 else
     echo "kibosh release tgz doesn't exists, downloading"
-    url=https://github.com/cf-platform-eng/kibosh/releases/download/${kibosh_version}/kibosh-release-0.0.75.tgz
+    url=https://github.com/cf-platform-eng/kibosh/releases/download/${kibosh_version}/kibosh-release-${kibosh_version}.tgz
     wget ${url} -O "${kibosh_release_path}"
 fi
 
